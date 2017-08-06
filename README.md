af40aeaeaa5ed081a9137e896ab9966d1a4be4db
tz70s <su3g4284zo6y7@gmail.com>
2017-08-06 21:34:16 +0800

    add the initial version
    still have many things to do, see issue.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pro-sy-kuo
 static web of lab
+
+Execution
+
+```bash
+# serve the http server
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+// Serve statics
+
+var express = require('express');
+var path = require('path');
+var app = express()
+
+// All static puts into ./static folder currently
+app.use('/',express.static(path.join(__dirname, 'static')));
+
+// index.html handler
+app.get('/', function(req, res) {
+    res.sendfile(path.join(__dirname, '/static/html/index.html'));
+});
+
+
+// server runs at :9111
+var server = app.listen(9111, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server running at ' + host + port);
+});
