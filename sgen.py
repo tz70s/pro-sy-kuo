a82229a2c6e4c0362a7478ccd50e68faa9213acd
tz70s <su3g4284zo6y7@gmail.com>
2017-08-09 12:00:04 +0800

    generate new static to docs
    modify some prefix in original static
    modify sgen.py for recusively detect files
    generate new static to docs

--- sgen.py
+++ sgen.py
@@ -5,11 +5,23 @@
 
 
 import sys
+from os import walk, path
 
 # File lists
 # The two file lists should be aligned.
-files = ["./static/html/award.html"]
-target_files = ["./award.html"]
+
+files = []
+for (dirpath, dirname, filenames) in walk("./static"):
+    for f in filenames:
+        if ".html" in f:
+            files.append(dirpath + "/" + f)
+
+# prefix of target files
+target_prefix = "./docs"
+target_files = []
+for f in files:
+    target_files.append(f.replace("./static", target_prefix))
+print(target_files)
 
 # Variables of parsing
 
@@ -25,12 +37,16 @@ def parse_args():
         exit(1)
     return original_prefix, new_prefix
 
-original_prefix, new_prefix = parse_args()
+def sgen():
+    original_prefix, new_prefix = parse_args()
 
-# parse the publications_ref into the appropriate html format
-for i in range(len(files)):
-	with open(files[i]) as f:
-		content = f.read()
-		new_content = content.replace(original_prefix, new_prefix)
-	with open(target_files[i], "w+") as f:
-		f.write(new_content)
+    # parse the publications_ref into the appropriate html format
+
+    for i in range(len(files)):
+	    with open(files[i]) as f:
+		    content = f.read()
+		    new_content = content.replace(original_prefix, new_prefix)
+	    with open(target_files[i], "w+") as f:
+		    f.write(new_content)
+
+sgen()
